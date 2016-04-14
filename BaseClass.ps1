d40bee252d19b2d0d894ed2db81372389d2f8a37
jm0477 <richie.jing@msn.cn>
2016-04-14 22:04:50 +0800

    BaseClass & Get0PStfs created

---
+++ BaseClass.ps1
@@ -0,0 +1,109 @@
+﻿#Load TFS APIs, and return TFS Server Collection
+function New-TFSconnection([string]$tfsUrl)
+{
+    #Load TFS PowerShell Snap-in
+    if ((Get-PSSnapIn -Name Microsoft.TeamFoundation.PowerShell -ErrorAction SilentlyContinue) -eq $null)
+    {
+        Add-PSSnapin Microsoft.TeamFoundation.PowerShell
+    }
+
+    #Load Reference Assemblies
+    [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.Client")  
+    [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.Build.Client")  
+    [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.Build.Common") 
+    [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.TeamFoundation.WorkItemTracking.Client") 
+ 
+    [string] $tfsCollectionUrl = $tfsUrl
+    #Get Team Project Collection
+    $teamProjectCollection = [Microsoft.TeamFoundation.Client.TfsTeamProjectCollectionFactory]::GetTeamProjectCollection($tfsCollectionUrl)
+ 
+    #Get Work Item Store object
+    $ws = $teamProjectCollection.GetService([type]"Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemStore")
+    return $ws
+
+}
+
+# Function will re-format the metadata from TFS, and return a ticket list
+function Format-TFSdata($ParentWorkItems)
+{
+    $result= New-Object 'System.Collections.Generic.List[psobject]'
+    foreach($p in $ParentWorkItems)
+    {
+        $items=$p.Fields
+        if($p.links.linktypeend.name -ne 'Parent')
+        {
+            $o= new-object psobject
+            foreach($item in $items)
+            {
+                $o | add-member -Name $item.name -Type NoteProperty -Value $item.Value
+            }
+            $result.Add($o)
+        } 
+    }
+    return $result
+}
+
+
+
+
+Function Format-Datetime($time)
+{
+    if($time -ne $null)
+    {
+         $Ftime = $time
+         $Ftime = get-date $time -Format "yyyy-MM-dd HH:mm:ss"
+         return "'"+$Ftime+"'"   
+    }
+    else
+    {
+        $Ftime = 'NULL'
+    }
+
+    return $Ftime
+}
+
+Function Format-Justification($T_Justification)
+{
+    if($T_Justification -ne $null)
+    {
+        $T_Justification = 0
+    }
+    else
+    {
+        $T_Justification = 1
+    }
+    return  "'" + $T_Justification + "'"
+}
+
+
+Function Replace-SingleQuote([string]$text)
+{
+    switch ($text)
+    {
+        {$_ -eq $null -or $_ -eq ""}
+        {
+            $text = 'NULL'
+            return $text
+        }
+        {$_ -ne $null}
+        {
+            $text = "'"+ $text.Replace("'","''") + "'"
+            return $text
+        }
+    }
+}
+
+
+
+Function Filter-Null($text)
+{
+    if($text -ne $null)
+    {
+        return $text
+    }
+    else
+    {
+        return "NULL"
+    }
+
+}
---
+++ Get-PStfs.ps1
@@ -0,0 +1,89 @@
+﻿######################################################################################
+# Script NAme: Get-PStfs.ps1                                                         #
+# Released:    April-14-2016                                                         #
+# Version:     1.0.0                                                                 #
+# Author:      Richie Jing                                                           #
+# Requires:    Visual Studio 2012 or higher version                                  #
+#              Microsoft Visual Studio Team Foundation Server Power Tools            #
+#              Mysql .NET connector                                                  #
+# Description: Query TFS api, insert into MySQL database for management              #
+#                                                                                    #
+######################################################################################
+
+# Import base class that the main application depends to run
+. "$env:HOMEPATH\Documents\GitHub\PStfs_management\BaseClass.ps1"
+
+[string]$tfsCollectionUrl = "http://10.20.201.160:8080/tfs/DefaultCollection"
+$ws                       = new-TFSconnection -tfsUrl $tfsCollectionUrl
+#$startDate= (get-lastweek)[0] 
+#$EndDate= (get-currentweek)[1]
+# Query all the Platform team's Change tickets
+$query                    = "SELECT [System.Id], [System.Title] FROM WorkItems " +
+                            "WHERE [Property Group] = 'Platform Services' " +
+                            #"WHERE [Property Group] = 'Network' " +
+                            "AND [Work Item Type] = 'Change'" +
+                            #"AND [Status] <> 'Canceled'" +
+                            #"AND [Status] <> 'Rejected'" +
+                            #"AND [Scheduled Start Date] > '$startDate'" +
+                            #"AND [Scheduled Start Date] < '$EndDate'" +
+                            "ORDER BY [System.Id]"
+
+$ParentItems              = $ws.query($query)
+
+#This assembly was provided by mysql .net connector
+[void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+
+$connectionStr = "Server=localhost;Uid=root;Pwd=jingming;database=tfs;pooling=true;min pool size=5;max pool size=512;connect timeout = 20;"
+#$querysql = "select * from user;"
+
+$connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+$connection.ConnectionString = $connectionStr
+$connection.Open()
+#$command = New-Object MySql.Data.MySqlClient.MySqlCommand($querysql, $connection)
+#$dataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($command)
+#$dataSet = New-Object System.Data.DataSet
+#$recordCount = $dataAdapter.Fill($dataSet)
+#$dataSet.Tables[0]
+#$connection.Close()
+
+
+
+
+#Construct the TFS data into SQL querycommand
+$formated_data = Format-TFSdata -ParentWorkItems $ParentItems
+$insert=""
+
+ForEach($x in $formated_data)
+{
+
+    [system.string]$T_id = $x.Id
+    [system.string]$T_title = Replace-SingleQuote -text $x.Title
+    [system.string]$T_Description = Format-Justification -T_Justification $x.'Description - 21ViaNet OE Project Change Management System'
+    [system.string]$T_Justification = Format-Justification -T_Justification $x.Justification
+    [system.string]$T_PreDeployment = Replace-SingleQuote -text $x.'Pre-Deployment'
+    [system.string]$T_PostDeployment = Replace-SingleQuote -text $x.'Post-Deployment Actions'
+    [system.string]$T_RollbackPlan = Replace-SingleQuote -text $x.'Rollback Plan'
+    [system.string]$T_SuccessCriteria = Replace-SingleQuote -text $x.'Success Criteria'
+    [system.string]$T_DeploymentMechanism = Replace-SingleQuote -text $x.'Deployment Mechanism'
+    [system.string]$T_PotentialImp = Replace-SingleQuote -text $x.'Potential Impact'
+    [system.string]$T_ExpectedImp = Replace-SingleQuote -text $x.'Expected Impact'
+    [system.string]$T_ScheduledStart = Format-Datetime -time $x.'Scheduled Start Date'
+    [system.string]$T_ScheduledEnd =  Format-Datetime -time $x.'Scheduled End Date'
+    [system.string]$T_ActualStart = Format-Datetime -time $x.'Actual Start Date'
+    [system.string]$T_ActualEnd = Format-Datetime -time $x.'Actual End Date'
+    [system.string]$T_Attachment  = '"' + $x.'Attached File Count' +'"'
+    [system.string]$T_History  = Replace-SingleQuote -text $x.History
+
+    $insert+= "INSERT INTO Ticket_RFC(T_id,T_title,T_Description,T_Justification,T_PreDeployment,T_PostDeployment,T_RollbackPlan,T_SuccessCriteria,T_DeploymentMechanism,T_PotentialImp,T_ExpectedImp,T_ScheduledStart,T_ScheduledEnd,T_ActualStart,T_ActualEnd,T_Attachment,T_History) VALUES($T_id,$T_title,$T_Description,$T_Justification,$T_PreDeployment,$T_PostDeployment,$T_RollbackPlan,$T_SuccessCriteria,$T_DeploymentMechanism,$T_PotentialImp,$T_ExpectedImp,$T_ScheduledStart,$T_ScheduledEnd,$T_ActualStart,$T_ActualEnd,$T_Attachment,$T_History);"
+}
+
+#call mysql object, and insert into Mysql database
+$insertcommand = new-object MySql.Data.MySqlClient.MySqlCommand
+$insertcommand.Connection = $connection
+$insertcommand.CommandText = $insert
+$insertcommand.ExecuteNonQuery()
+
+#===============================================================================
+# trying to get more properties of the customized objects
+
+$single = $ParentItems[0]
